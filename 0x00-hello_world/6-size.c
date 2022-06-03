981ef30b1fe295ab0ef3bca1a55cd5bd71d9c8d5
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-06-03 09:28:36 -0700

    number 100

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+                                                /**
+ * main - Entry point
+ *
+ * Description: prints size of various types.
+ *
+ * Return: 0 if Success                          */
+int main(void)
+{
+
