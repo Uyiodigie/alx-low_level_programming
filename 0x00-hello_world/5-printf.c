455cd17ea61df579c4c6d01c9f0798defc7b22dd
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-06-03 09:10:29 -0700

    number five

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
- * */
+ * Description: prints string
+ *
+ * Return: 0 if Success
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
