44b5a78beb4d308db528bbd9776ee285e1c1df94
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-06-03 12:00:09 -0700

    number 6;

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,7 @@
-#!/bin/bash
+#include <stdio.h>
+                                                /**                                              * main - Entry point
+ *
+ * Description: prints string in stderror.
+ *
+ * Return: 0 if success                          */                                             int main(void)
+{
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
-                                                /**
+
+/**
  * main - Entry point
  *
- * Description: prints size of various types.
+ * Description: print the type of a string on the stdout.
  *
- * Return: 0 if Success                          */
+ * Result: Always 0 (Success)
+ */
 int main(void)
 {
-
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
+return (0);
+}
