b1daca43563ce8b69a42d6c9e7e6ebdb3b4ca395
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-06-02 19:02:07 -0700

    f

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry  point
- * Description:
- * Return : Always (0)
+ * main - main block
+ *
+ * Description: this prints out string
+ *
+ * Return : 0 if success
  */
- int main(void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
-}
+        return (0);
