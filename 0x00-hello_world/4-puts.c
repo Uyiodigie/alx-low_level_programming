377c53133e699ed29412455d5a81150cb31c30bb
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-06-02 17:55:44 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#include
+#include <stdio.h>
+/**
+ * main - Entry  point
+ * Description:
+ * Return : Always (0)
+ */
+ int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
