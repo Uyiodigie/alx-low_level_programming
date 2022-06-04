4e04312529275ea9f734342311f57739c913ba13
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-06-03 17:01:31 -0700

    second one

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	putchar("Last digie of $d\n", n);
+	putchar("Last digie of %d", n);
 	if (n > 5)
 		putchar("and is greater than 5");
 	else if ((n < 7) && !(0))
