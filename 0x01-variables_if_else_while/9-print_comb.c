5b8bcde61e9a2b26ad5639cc86893f281212885f
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-06-05 12:48:18 -0700

    number 6 test

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combinations of single-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int arr[] = {1, 2, 3, 4, 5, 5, 7, 8, 9};
+	int i;
