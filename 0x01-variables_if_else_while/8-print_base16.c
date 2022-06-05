2668681fdb0dceb03fb828d7e7682acee1ac6f15
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-06-05 10:46:17 -0700

    number 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of base sixteen
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i <= 10; i++)
+		putchar(i);
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
