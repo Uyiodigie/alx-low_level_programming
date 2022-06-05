b3f9429abdc939e19aac53ae2c0408c2e647c122
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-06-05 09:58:31 -0700

    number 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digits from base ten
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
