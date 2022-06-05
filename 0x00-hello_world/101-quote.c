37d1a6ef5a6ac648f0070dc2bae71661482416e8
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-06-05 07:36:16 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-                                                /**                                              * main - Entry point
+/**
+ * main - prints string to stderr
  *
- * Description: prints string in stderror.
- *
- * Return: 0 if success                          */                                             int main(void)
+ * Return: 1
+ */
+int main(void)
 {
+	int start = 1;
+	int end = start + 58;
+char msg(60) = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(msg, end, start, stderr);
+	return (1);
+}
