42b45cab9e0bbb4a346d4d6b25dd8a8933c6ca56
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-06-06 07:34:06 -0700

    number 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - displays 3 combo numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 48;
+	int b = 49;
+	int c = 50;
+	int counter = 48;
+
+	while (a < 56)
+	{
+		while (b > a)
+		{
+			while (c != 58)
+			{
+				if (c > b)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a < 55)
+					{
+						putchar(',');
+
+						putchar(' ');
+					}
+				}
+				c++;
+			}
+			c = 50;
+		}
+		b++;
+	b = 49;
+	a++;
+	counter++;
+	}
+	putchar('\n');
+	return (0);
+}
