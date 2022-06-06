e662563fef421f997aa365f4d5bc3dfcc9eb3acc
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-06-06 07:22:55 -0700

    number 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible different combinations of two digit numbers
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int a = 48;
+	int b = 48;
+	int counter = 48;
+
+	while (counter != 58)
+	{
+		while (b != 58)
+		{
+			if (b > a)
+			{
+				putchar(a);
+				putchar(b);
+				if (a != 57)
+				{
+					putchar(',');
+
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		a++;
+		b = 48;
+		counter++;
+	}
+	putchar('\n');
+	return (0);
+}
