b087b6a40520be64eae16d66c0a9c7a911713e0c
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-07 15:41:45 -0700

    number 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main -prints the alphabet in lowercase followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i < 'z'; i++)
+		putchar(i);
+	putchar('\n');
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Contains tasks for "Funcions, nested loops".
