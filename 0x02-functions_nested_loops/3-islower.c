65a3776a974d26514aced1e926f7e47695d42c09
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-07 19:15:22 -0700

    number 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - checks for lowercase character
  *
- * @c - it is the integer parameter of the function
+ * @c: it is the integer parameter of the function
  *
  * Return: Always 0 (Success)
  */
