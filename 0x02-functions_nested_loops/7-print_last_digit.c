4c312ca82d22681b22f10e165fba8cda6e7b70da
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-08 16:29:30 -0700

    number 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - prints the value of the last digit
+ *
+ * @i: integer parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int i)
+{
+	int modu = i % 10;
+
+	if (i > 0)
+		-putchar(modu);
+	return (0);
+}
