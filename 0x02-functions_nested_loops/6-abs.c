ec4894f35e7db17e089ca1ddf3ae3128d8d27d33
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-08 15:46:58 -0700

    number 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - computes absolute value of integer
+ *
+ * @i: integer parameter
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int i)
+{
+	if (i > 0)
+	{
+		return (i);
+	}
+	else if (i < 0)
+	{
+		return (i);
+	}
+	else
+		return (i);
+}
