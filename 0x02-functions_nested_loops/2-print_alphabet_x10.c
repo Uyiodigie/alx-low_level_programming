2c3cf43a1ee8b58c123dcc6ef3a4fe9856e5e3f8
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-07 17:24:30 -0700

    number 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	while (i <= 10)
+	{
+		for (j = 'a'; j < 'z'; j++)
+			_putchar(j);
+	}
+	i++;
+	_putchar('\n');
+}
