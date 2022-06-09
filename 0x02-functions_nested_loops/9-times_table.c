94ceb020f17efe2112d31d0e7aab6804e98a38e8
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-08 21:51:42 -0700

    s

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,16 +3,24 @@
 /**
  * times_table - prints the times table
  *
- * Return: Always 0 (Success)
+ * Return: table
  */
 void times_table(void)
 {
-	int i = 0,j,k;
+	int i, j, k;
 
-	while (i <= 9)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 0; j <= 9; ++j)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (j = 1; j <= 9; j++)
 		{
-			j = j + 1;
-		}
-		i++;
+			d = (i * j);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else if (cone < 9)
+			_putchar((d % 10) + '0')
+	
