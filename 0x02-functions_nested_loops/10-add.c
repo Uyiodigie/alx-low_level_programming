1b182ce5f9ceb1d3ee2a3c84c68502bafe6caee7
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-08 19:28:48 -0700

    number 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @i: integer parameter
+ *
+ * @j: integer parameter
+ *
+ * Return: result
+ */
+int add(int i, int j)
+{
+	int result;
+
+	result = i + j;
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int i = 0,j,k;
+
+	while (i <= 9)
+	{
+		for (j = 0; j <= 9; ++j)
+		{
+			j = j + 1;
+		}
+		i++;
