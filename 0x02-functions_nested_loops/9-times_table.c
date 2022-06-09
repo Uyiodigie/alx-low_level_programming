6015e4dc9aa638a6442bd78747022b37c2c8c0a7
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-08 22:04:54 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 
 	for (row = 0; row < 9; row++)
 	{
-		for (column = 0; colummn <= 9; column++)
+		for (column = 0; column <= 9; column++)
 		{
 			product = row * cloumn++;
 			tens = product / 10;
