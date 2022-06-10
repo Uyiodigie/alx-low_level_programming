acbfc31911778a2086dd5c27e2086abaa77787b9
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-10 13:45:48 -0700

    number 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,16 +11,16 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n >= 0)
+	if (n > 0)
 	{
 		for (i = 0; i != n; i++)
 		{
-			_putchar('\\');
-			_putchar('\n');
 			for (j = 0; j < i; ++j)
 			{
 				_putchar(' ');
 			}
+		_putchar(92);
+		_putchar('\n');
 		}
 	}
 	else
