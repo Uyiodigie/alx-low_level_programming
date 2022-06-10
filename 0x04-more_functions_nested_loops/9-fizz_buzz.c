5abd47cce85a98fe1b3fbbd033bcd20194a20873
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-10 16:33:53 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,19 +8,19 @@
  */
 int main(void)
 {
-	int i = 1;
+	int i;
 
-	for (; i < 100; i++)
+	for (i = 0; i < 100; i++)
 	{
-		if (((i % 3) == 0) && (i % 5) == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
-		else if ((i % 3) == 0)
+		else if (i % 3 == 0)
 			printf("Fizz ");
-		else if ((i % 5) == 0)
+		else if (i % 5 == 0)
 			printf("Buzz ");
 		else
 			printf("%d ", i);
 	}
-	putchar("Buzz\n");
+	printf("Buzz\n");
 	return (0);
 }
