dc58a496d53992e4d4b9b6e68b048e35c5238ae4
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-10 19:16:29 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,7 +11,11 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if (size > 0)
+	if (size <= 7)
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		for (i = 0; i >= size; i++)
 		{
@@ -26,6 +30,4 @@ void print_triangle(int size)
 		}
 
 	}
-	else
-		_putchar('\n');
 }
