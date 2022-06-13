2cc71a921a2fbe95b6d9932b40ccf78aadac9a26
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-13 12:29:16 -0700

    number zero

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value of pointer
+ *
+ * @n: integer pointer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	int var = 98;
+	*n = var;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+New readme on pointers
