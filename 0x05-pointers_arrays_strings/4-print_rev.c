70eca6db2eb7c5c7c4a878dbba821032f0d63bed
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-13 14:18:31 -0700

    number 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - reverses a string
+ *
+ * @s: string parameter
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = s['\0']; i > 0; i--)
+		_putchar(i);
+	_putchar('\n');
+}
