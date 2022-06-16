e7c2b7b390141ce1a36c855b036bb8b9248d17c2
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-15 17:00:58 -0700

    number one

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,19 +9,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int len1 = *dest, len2 = *src, i = 0, j = 0;
+	int lenO = *dest, lenT = *src, i = 0, j = 0;
 
-	while (*(dest + i) != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (*(src + j) != '\0')
+	while (src[j] != '\0')
 	{
 		j++;
 	}
-	for (i = 0; i <= len2; i++)
+	for (i = 0; i <= lenT; i++)
 	{
-		dest[len1 + i] = '\0';
+		dest[lenO + i] = '\0';
 	}
 
 	_putchar('\n');
