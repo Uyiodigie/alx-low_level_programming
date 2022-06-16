e9070dfd0136e00b05e3ded1d907ab023e9d4afe
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-16 13:16:55 -0700

    number 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: destination string
+ * @s2: source string
+ * Return: pointer to s1
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
