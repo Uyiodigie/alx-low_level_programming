650eb5a08dd2a5e6b65cbb08ed08efd47e10291e
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-16 16:26:08 -0700

    number

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,17 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, fail;
+	int i = 0, j = 0;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != 0; i++)
+	while (j == 0)
 	{
-		if (s1[i] != s2[i])
-		{
-			fail = 1;
+		if (s1[i] != '\0' && s2[i] != '\0')
 			break;
-		
-		}
+		j = s1[i] - s2[i];
+		i++;
 	}
-	if (fail != 1)
-		return (s1);
+	return (j);
 }
