30e0ce693b705e771c3684b08ca85b9be62874a7
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-17 12:13:16 -0700

    number 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string parameter
+ * Return: pointer to destination
+ */
+char *leet(char *s)
+{
+	int i = 0, j;
+	int lower[] = {'a', 'e', 'o', 't', 'l'};
+	int upper[] = {'A', 'E', '0', 'T', 'L'};
+	int numbers[] = {52, 51, 48, 55, 49};
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == lower[i] || s[i] == upper[i])
+			{
+				s[i] = numbers[i];
+				break;
+			}
+		}
+		i++;
+	}
+	return (s);
+}
