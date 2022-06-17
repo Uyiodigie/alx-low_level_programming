fdebb5c8281583d80255d0acd8f51cc60b08e4fd
Odigie Uyioghosa Ernest <82983355+Uyiodigie@users.noreply.github.com>
2022-06-17 05:51:56 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,6 @@
 #include "main.h"
 
 /**
-* cap_string - capitalizes strings
-* @s: input string.
-* Return: the pointer to dest.
-*/
-char *cap_string(char *s)
-{
-	int i = 0, j;
-	int strs[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-
-	if (s[i] >= 'a' && s[i] <= 'z')
-		s[i] = s[i] - 32;
-	i++;
-	while (s[i] != '\0')
-	{
-		for (j = 0; j < 13; j++)
-		{
-			if (s[j] == strs[j])
-			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-					s[i + 1] = s[i + 1] - 32;
-				break;
-			}
-		}
-		i++;
-	}
-	return (s);
-}
-#include "main.h"
-
-/**
 
  * cap_string - capitalizes all words of a string
 
