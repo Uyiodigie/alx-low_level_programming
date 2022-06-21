78627b928106515659f9746c5693c6f75d919bfc
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-20 18:46:52 -0700

    number 6

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string pointer paramter
+ * @c: string parameter
+ */
+char *_strchr(char *s, char c)
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any set of bytes
+ * @s: pointer to s
+ * @accept: pointer to accept
+ * Return: pointer to the byte in s
+ */
+char *_strpbrk(char *s, char *accept)
