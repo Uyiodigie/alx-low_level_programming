36a8eae497598b11371d96b2860c23f19a3752d9
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-20 20:47:36 -0700

    number 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - prints the sum of two diagonal numbers
@@ -8,4 +9,13 @@
  */
 void print_diagsums(int *a, int size)
 {
+	int i, var1 = 0, var2 = 0;
 
+	for (i = 0; i < size; i++)
+	{
+		var1 += *(a + (size * i + i));
+		var2 += *(a + (size * i + size - 1 - i));
+	}
+	printf("%d, ", var1);
+	printf("%d\n", var2);
+}
