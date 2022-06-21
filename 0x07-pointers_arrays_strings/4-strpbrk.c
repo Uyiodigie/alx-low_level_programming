d2d013471cfa6128ea95cca119b1836d7da17395
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-20 20:29:49 -0700

    number 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strpbrk - searches a string for any set of bytes
@@ -7,3 +8,17 @@
  * Return: pointer to the byte in s
  */
 char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of two diagonal numbers
+ * @a: pointer
+ * @size: size of the square
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+
