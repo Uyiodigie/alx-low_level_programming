cded544a4a2cb30c5445084159ad38d6d77f1473
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-20 20:37:57 -0700

    number 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,6 +3,27 @@
 /**
  * _strspn - gets the length of a prefix substring
  * @s: string pointer paramter
- * @c: string parameter
+ * @accept: string parameter
+ * Return: length
  */
-char *_strchr(char *s, char c)
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int c = 0;
+	char *t = accept;
+
+	while (*s++)
+	{
+		while (*accept++)
+		{
+			if (*(s - 1) == *(accept - 1))
+			{
+				c++;
+				break;
+			}
+		}
+		if (!(*--accept))
+			break;
+		accept = t;
+	}
+	return (c);
+}
