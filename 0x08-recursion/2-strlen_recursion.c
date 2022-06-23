69de66a1b5d3fdab632a23b769b0416a8d1be7cc
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-22 17:15:45 -0700

    number 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,10 +7,9 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
-		_putchar(*s);
-		_strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(++s));
 	}
-	return (*s);
+	return (0);
 }
