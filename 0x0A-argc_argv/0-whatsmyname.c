75c2e93d60c84e7f0f471d432f5d40ba398c47b4
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-24 14:04:45 -0700

    created files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#incule "main.h"
+
+/** main - prints it's name followed by a new line
+ * @argc: argument count
+ * @argc: argument vect0r
+ * Return: 0 if success
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+0x0A.C - argc, argc
+
+A new project on argument count and argument vectors
