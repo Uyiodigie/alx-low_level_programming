665af80093193c33540be68735567b98615b810c
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-24 14:26:35 -0700

    number 0 test

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,11 @@
 
 /**
  * main - prints it's name followed by a new line
- * @argc: argument count
  * @argv: argument vect0r
  * Return: 0 if success
  */
-int main(int argc, char *argv[])
+int main(char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
