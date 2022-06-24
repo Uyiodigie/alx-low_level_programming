3ad16aa716751525679c6b1048417c0d74239979
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-24 14:18:19 -0700

    number 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
-#incule "main.h"
+#include "main.h"
 
-/** main - prints it's name followed by a new line
+/**
+ * main - prints it's name followed by a new line
  * @argc: argument count
- * @argc: argument vect0r
+ * @argv: argument vect0r
  * Return: 0 if success
  */
 int main(int argc, char *argv[])
 {
-	if (argc == 1)
-
-
+	if (argc == 1 && argv[0])
+		_putchar(*argv[0]);
+	return (0);
+}
