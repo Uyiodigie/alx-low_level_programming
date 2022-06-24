b037f689554c850509f802e9cd16cdfcf2078e45
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-24 16:18:51 -0700

    number 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int i, result;
+
+	for (i = 0; i < argc; i++)
+	{
+		if (argc != 2)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			printf("%s", argv[i]);
+			result *= atoi(argv[i]);
+		}
+	}
+	return (0);
+}
