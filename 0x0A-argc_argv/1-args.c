67ccf4e37da47391185766dec1c43942e8b530a6
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-24 15:12:48 -0700

    number 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,13 +4,14 @@
 /**
  * main - prints the number of arguments passed
  * @argc: argument count
+ * @argv: argument vector
  * Return: 0 success
  */
-int main(int argc)
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
-		printf("%d", argc);
+		printf("%s\n", argv[i]);
 	return (0);
 }
