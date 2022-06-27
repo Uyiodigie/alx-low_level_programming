749c28d69082476bd44c6064dcf5ab9f788c1c44
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-27 12:29:54 -0700

    number 1

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for upper case
+ * @c: integer parameter
+ */
+int _isupper(int c)
---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory
+ * @s: target
+ * @b: constant byte
+ * @n: size
+ */
+char *_memset(char *s, char b, unsigned int n)
