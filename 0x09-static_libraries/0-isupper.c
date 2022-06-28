b60664f2d302087e9d03696de8d673c550f1a6b5
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-06-28 04:23:25 -0700

    number 1

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -6,3 +6,8 @@
  * Return: 0 success
  */
 int _isupper(int c)
+{
+	if ((c >= 'A') && (c < 'Z'))
+		return (1);
+	return (0);
+}
--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -5,6 +5,14 @@
  * @s: target
  * @b: constant byte
  * @n: size
- * Return: 0 success
+ * Return: valuse of s
  */
 char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
+}
