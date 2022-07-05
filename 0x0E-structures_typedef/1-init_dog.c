5ac00ba6b993298b66b062e9dc0ff2ee3414c407
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-07-04 17:50:48 -0700

    number 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,15 +7,17 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
+ * Description: initializes the variables in the struct dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
-	else
+
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
+	else
+		return;
 }
