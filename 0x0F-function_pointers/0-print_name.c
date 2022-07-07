bb956f78989d9a45d8a08fa265d3e2c928c51790
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-07-06 17:01:18 -0700

    number 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
  * print_name - prints a name
@@ -12,6 +11,5 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
 		return;
-	else
-		f(name);
+	f(name);
 }
