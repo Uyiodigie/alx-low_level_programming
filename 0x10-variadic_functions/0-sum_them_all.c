0ed998669c49f6404413ffe447f86e7280223e0f
Uyiodigie <odigieuyioghosa256@gmail.com>
2022-07-07 14:47:36 -0700

    number 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,10 +13,10 @@ int sum_them_all(const unsigned int n, ...)
 	int add = 0;
 
 	if (n == 0)
-		return(0);
+		return (0);
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
-		add += var_arg(valist, int);
+		add += va_arg(valist, int);
 	va_end(valist);
 	return (add);
 }
