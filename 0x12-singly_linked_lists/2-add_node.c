118de9d80993060e32856334b6c9f38e02a1c631
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-07-14 17:09:40 -0700

    number 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,6 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 	temp = malloc(sizeof(list_t));
 	if (temp == NULL)
 		return (NULL);
+	temp->str = strdup(str);
 	for (i = 0; str[i]; i++)
 		;
 	temp->len = i;
