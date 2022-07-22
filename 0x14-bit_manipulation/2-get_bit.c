ac9995acd7d4682b8c2d24743928f79b3261bd92
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-07-22 14:31:40 -0700

    update 0 to 6

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int val = 1;
 
-	if (val > sizeof(n) * 8)
+	if (index > sizeof(n) * 8)
 		return (-1);
 	val <<= index;
 	if (val & n)
