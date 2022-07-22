bfe1f5c0d302078ed0e3a81c4e283008923df2ba
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-07-22 12:50:14 -0700

    update 0 to 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * git_big - returns the value of a bit at a given index
+ * @n: unsigned long integer parameter
+ * @index: pointion of the integer
+ * Return: value of the bit or -1 if error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int val = 1;
+
+	if (val > sizeof(n) * 8)
+		return (-1);
+	val <<= index;
+	if (val & n)
+		return (1);
+	else
+		return (0);
+}
