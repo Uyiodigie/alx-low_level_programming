88d8aa5a4f08959ad79e9783851c24cfbd803f4c
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-09-28 13:34:06 -0700

    number 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: the size of the array
+ * Return: a pointer to the hash table, NULL if fail
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *table = NULL;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+	return (table);
+}
